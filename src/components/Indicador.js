96606baea6487d32d5eca70de64132d81c8bd548
Jaczibeth <jaczicruz@gmail.com>
2025-10-31 10:36:33 -0600

    Modificacion implementando paper en nuestro indicador guiandonos de la documentacion oficial

--- src/components/Indicador.js
+++ src/components/Indicador.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { ActivityIndicator, MD2Colors } from 'react-native-paper';
 
 const Indicador = () => (
-  <ActivityIndicator animating={true} color={MD2Colors.red800} />
+  <ActivityIndicator animating={true} color={MD2Colors.blueA200} size={'large'} />
 );
 
 export default Indicador;
