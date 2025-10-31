8eceaa915255e49d60e57ddd142c6d44aebdb3ae
Jaczibeth <jaczicruz@gmail.com>
2025-10-31 10:50:02 -0600

    Aplicacion de card de informacion y icono

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,23 @@
-import { StyleSheet, Text, View, } from 'react-native'
-
-import { Appbar } from 'react-native-paper';
+import { View, } from 'react-native'
+import { Appbar , Card, Text,Icon, MD3Colors} from 'react-native-paper';
 export default function Home() {
     return (
         <View   >
             <Appbar.Header>
                 <Appbar.Content title="Bienvenidos" />
             </Appbar.Header>
+            <Card>
+    <Card.Content>
+      <Text variant="titleLarge">TlaxTec</Text>
+      <Text variant="bodyMedium">Card de informacion</Text>
+    </Card.Content>
+  </Card>
+
+  <Icon
+    source="camera"
+    color={MD3Colors.error50}
+    size={20}
+  />
         </View>
     );
 }
