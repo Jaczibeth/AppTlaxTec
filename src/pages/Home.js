776144ebf012bd26bf40f28492cf47c6fdb6e6fa
Jaczibeth <jaczicruz@gmail.com>
2025-10-31 11:49:14 -0600

    Usando SafeAreaProvider

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,30 @@
 import { View, } from 'react-native'
-import { Appbar , Card, Text,Icon, MD3Colors} from 'react-native-paper';
+import { Appbar , Card, Text,Icon, MD3Colors, Surface, DefaultTheme, PaperProvider} from 'react-native-paper';
 import Lista from '../components/Lista';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+
+const theme = {
+    colors:{
+        ...DefaultTheme.colors,
+        primary:'#3f51b5',
+        accent:'#f50057',
+        background:'#f1e0e0de',
+        Surface:'#e4cbcbff',
+        text:'#000000'
+    }
+}
+
 export default function Home() {
     return (
-        <View   >
-            <Appbar.Header>
-                <Appbar.Content title="Bienvenidos" />
-            </Appbar.Header>
-            <Card>
-    <Card.Content>
-      <Text variant="titleLarge">TlaxTec</Text>
-      <Text variant="bodyMedium">Card de informacion</Text>
-    </Card.Content>
-  </Card>
-
-  <Icon
-    source="camera"
-    color={MD3Colors.error50}
-    size={20}
-  />
-  <Lista/>
-        </View>
+    <SafeAreaProvider>
+        <PaperProvider theme={theme}>
+            <SafeAreaView style={{backgroundColor:theme.colors.background, flex:1}}>
+  <Appbar.Header>
+   
+    <Appbar.Content title="Aplicaciones Moviles Jaczibeth" />
+  </Appbar.Header>
+            </SafeAreaView>
+        </PaperProvider>
+    </SafeAreaProvider>
     );
 }
