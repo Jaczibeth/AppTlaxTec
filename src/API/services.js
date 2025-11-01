e9c7304578408f211a172354ecc3f2b536d26275
Jaczibeth <jaczicruz@gmail.com>
2025-11-01 00:22:37 -0600

    Actualizacion de peticiones put, post y delete

--- src/API/services.js
+++ src/API/services.js
@@ -3,7 +3,7 @@ import { Platform } from 'react-native';
 const BASE_URL = Platform.select({
     ios: 'http://localhost:8080',
     android: 'http://10.0.2.2:8080',
-    default: 'http://192.168.0.1:8080',
+    default: 'http://192.168.0.101:8080',
   });
 const http = axios.create({
     baseURL: BASE_URL,
@@ -18,8 +18,18 @@ http.interceptors.request.use(
 }
 );
 export const AlumnosService = {
-    traerAlumnos:() => http.get('/alumnos/traer-alumnos').then((res) => res.data),
+    traerAlumnos:() =>
+         http.get('/alumnos/traer-alumnos').then((res) => res.data),
+    traerAlumnosId: (id) => 
+    http.get(`/alumnos/traer-alumno/${id}`).then((res) => res.data),
+    insertarAlumno:(alumnos) => 
+        http.post('/alumnos/insertar-alumnos', alumnos).then((res) => res.data),
+    actualizarAlumno:(id, alumnos) => 
+        http.put(`/alumnos/editar-alumnos/${id}`, alumnos).then((res) => res.data),
+    eliminarAlumno:(id) => 
+        http.delete(`/alumnos/eliminar-alumnos/${id}`).then((res) => res.data),
 }
+
 
 export default{
     AlumnosService, 
